e5f8e44af2895af41f88d02c2935ec63cf7d4671
Pavan <pvn.gupta.2009@gmail.com>
2024-03-09 10:37:13 +0000

    calculating high

--- nifty/nifty_monthly.py
+++ nifty/nifty_monthly.py
@@ -1,9 +1,14 @@
+"""
+In this analysis I'll go through nifty daily data and analys the effect of topped up SIPs during down months
+
+"""
 import pandas as pd
 
 DATA_DIR = 'nifty/data/'
-
 NIFTY_50_10 = pd.read_csv(DATA_DIR + 'NIFTY_50_Historical_PR_01012000to31122010.csv')
 NIFTY_50_24 = pd.read_csv(DATA_DIR + 'NIFTY_50_Historical_PR_01012011to29022024.csv')
+
+SIP = 1000
 
 nifty_df = pd.concat([NIFTY_50_10, NIFTY_50_24], ignore_index=True)
 
@@ -22,6 +27,16 @@ nifty_df['Date'] = pd.to_datetime(nifty_df['Date'])
 nifty_df.sort_values(by="Date", inplace=True)
 nifty_df = nifty_df.reset_index(drop=True)
 
-print(nifty_df)
+
+running_high = 0
+for i, row in nifty_df.iterrows():
+    if row['High'] > running_high:
+        running_high = row['High']
+        print(row['Date'], running_high)
+    # print(row)
+
+# print(running_high)
+
+# print(nifty_df)
 
 # print("Hello from github codespace.")
